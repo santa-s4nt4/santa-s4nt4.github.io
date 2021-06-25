20b2a068ba299e368999ac2396c4b41b595e0444
santa <santanaruse@gmail.com>
2021-06-26 03:14:11 +0900

    add: デフォルトのコンポーネントを追加

---
+++ components/none.vue
@@ -0,0 +1,28 @@
+<template>
+</template>
+
+<style>
+template {
+  font-family: "JetBrains Mono";
+  font-size: 10px;
+  color: #ffffff;
+}
+
+.container {
+  background-color: black;
+  min-height: 100vh;
+  text-align: left;
+  padding-top: 1rem;
+  padding-left: 1rem;
+}
+
+h1 {
+  font-family: "JetBrains Mono";
+  font-weight: 500;
+  display: block;
+  font-size: 1.2rem;
+  color: #ffffff;
+  letter-spacing: -0.05rem;
+  padding-bottom: 1rem;
+}
+</style>
