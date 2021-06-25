b3f71b412b111185437ac80ffd6c4ae205ffb9de
santa <santanaruse@gmail.com>
2021-06-26 04:58:54 +0900

    add: save the metroのcomponentを追加

---
+++ components/works/save_metro.vue
@@ -0,0 +1,135 @@
+<template>
+<div>
+  <div class="work">
+    <div class="title">
+      <p>visual system for Save The Metro Compilations</p>
+    </div>
+    <img
+    class="magnifiable"
+    src="https://lh3.googleusercontent.com/QlnS59pO3nq3EMc_0q-hpX5Wn1D7lp1MW5DJRh--4_9znb8H0hj7mkpwPUuxFNxsjX5Vr_QlbXZfrJJfeUgiNpQPralPsCRv2FjeMeiigNeu-ia_Zh5S3ypihA_gIzeSjfPv5qjNrrs=w2400"
+    itemprop="image"
+    width="80%"/>
+    <br>
+    <img
+    class="magnifiable"
+    src="https://lh3.googleusercontent.com/aMzRBxUPGB1FhEMufTVkxbdUsp1TOisH2qvcf4sqZnsDHGBGJpHCULON1DdLbvJ5FnX2yFrfj89-AYdJMhVjslcCxk0uYTNFgf0EqZZgEgWqBZqDDtAYOYWr-lqUAz1WN3pbjrc-uEI=w2400"
+    itemprop="image"
+    width="80%"/>
+    <br>
+    <img
+    class="magnifiable"
+    src="https://lh3.googleusercontent.com/1dLsdYkWmmOSDTh6MFqseJM7VyiMQ9_M9nDK2DFXvTzQ1OUsuDP45cTPUotY8aQeC6_eMLf-sKGrlQvT5wCmhj83RGMre5THsewffcoQqjrj2AcIhgjm41MFHvcB_xmCHCp0MPpFTSw=w2400"
+    itemprop="image"
+    width="80%"/>
+    <br>
+        <div class="discription">
+          <p width=60%>
+            <br />
+            2020/09/25に<a href="https://www.twitch.tv/rhizomatiks">twitch.tv/rhizomatiks</a>で配信された<a
+            href="https://twitter.com/rhizomatiks/status/1308730110661414912?s=20"
+            >Playing Tokyo Vol.11</a
+          >
+          でのVJに使用したVisual System．
+          </p>
+        </div>
+        <div class="caption">
+          <p>
+          Tool : TouchDesigner / GLSL
+          </p>
+          <p>
+            Sound :
+          <a href="http://www.hiramatsutoshiyuki.com/index/HIRAMATSU_TOSHIYUKI.html" target="_blank">HIRAMATSU TOSHIYUKI</a
+          >
+          </p>
+          <p>
+          Link :
+          <a href="https://twitter.com/metro_kyoto/status/1273953213180375040?s=20" target="_blank"
+            >Kyoto Metro</a
+          >
+          </p>
+          <p>
+            Source Code : <a href="https://github.com/santa-s4nt4/200627_metro" target="_blank">GitHub</a>
+          </p>
+        </div>
+  </div>
+</div>
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500&display=swap");
+
+template {
+  font-family: "JetBrains Mono";
+  font-size: 10px;
+}
+
+.work {
+  width: 60%;
+  padding-bottom: 3rem;
+}
+
+.work p {
+  font-family: "JetBrains Mono";
+  font-weight: 500;
+  display: block;
+  font-size: 1rem;
+  color: #ffffff;
+  letter-spacing: -0.05rem;
+}
+
+.work a {
+  text-decoration: none;
+  color: #9e9e9e;
+}
+
+.work a:hover {
+  color: #ffffff;
+}
+
+.work mark {
+  background-color: #ffffff;
+  font-family: "JetBrains Mono";
+  font-weight: 500;
+  font-size: 0.8rem;
+}
+
+.title p {
+  font-family: "JetBrains Mono";
+  font-weight: 500;
+  display: block;
+  font-size: 1.2rem;
+  color: #ffffff;
+  letter-spacing: -0.05rem;
+}
+
+.discription {
+  padding-bottom: 1.6rem;
+}
+
+.discription p {
+  font-family: 'Noto Sans JP', sans-serif;
+  font-weight: 400;
+  font-size: 1rem;
+  color: #ffffff;
+}
+
+.caption p {
+  padding-bottom: 0.5rem;
+}
+
+.video {
+  position: relative;
+  padding-bottom: 56.25%;
+  height: 0;
+  overflow: hidden;
+}
+
+.video iframe {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+}
+
+</style>
