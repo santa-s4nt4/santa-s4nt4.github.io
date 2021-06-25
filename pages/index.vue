6de1cb8effbdf2e0b58f8f240808c5d6cde85577
santa <santanaruse@gmail.com>
2021-06-26 01:00:26 +0900

    add: テキストを追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,31 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        santa-s4nt4
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <div class="title">
+      <p>S4NT4.GRAPHICS</p>
+    </div>
+    <div class="links">
+      <a href="https://twitter.com/santa_s4nt4" target="_blank">
+        &#x1f5e3;
+      </a>
+      <a href="https://www.instagram.com/santa_s4nt4/" target="_blank">
+        &#x1f4f7;
+      </a>
+      <a href="https://github.com/santa-s4nt4" target="_blank">
+        &#x1f528;
+      </a>
+    </div>
+    <div class="person_discription">
+      <p>
+        Santa is a visual programmer based in Tokyo.
+        <br> Favorite: visual performance, generative art, installation...
+      </p>
+    </div>
+    <div class="works">
+      <p>
+        Noticeable works: 
+        <br>
+        Sonic-Aquarium, holarchy, Playing Tokyo Vol.11, Save The Metro Compilations, Algorave Tokyo
+      </p>
     </div>
   </div>
 </template>
@@ -32,42 +35,52 @@ export default {}
 </script>
 
 <style>
+template {
+  font-size: 10px;
+}
+
+@font-face {
+  font-family: "JetBrainsMono";
+  src: url("/assets/font/JetBrainsMono-ExtraBold.woff2") format("woff2");
+}
+
 .container {
-  margin: 0 auto;
+  background-color: black;
   min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  text-align: left;
 }
 
 .title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: "JetBrainsMono";
   display: block;
   font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+  font-size: 1.6rem;
+  color: #ffffff;
+  letter-spacing: 0.15rem;
 }
 
 .links {
-  padding-top: 15px;
+  display: block;
+  font-weight: 300;
+  font-size: 1.6rem;
+  color: #ffffff;
+  letter-spacing: 0.15rem;
 }
+
+.person_discription {
+  display: block;
+  font-weight: 300;
+  font-size: 1.6rem;
+  color: #ffffff;
+  letter-spacing: 0.15rem;
+}
+
+.works {
+  display: block;
+  font-weight: 300;
+  font-size: 1.6rem;
+  color: #ffffff;
+  letter-spacing: 0.15rem;
+}
+
 </style>
