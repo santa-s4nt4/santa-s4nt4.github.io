1061cbe5c90c8bcf407572bacd664fe2f8eedd14
santa <santanaruse@gmail.com>
2021-06-26 04:51:17 +0900

    add: playing tokyoのcomponentを追加

---
+++ components/works/pt_11.vue
@@ -0,0 +1,147 @@
+<template>
+<div>
+  <div class="work">
+    <div class="title">
+      <p>visual system for Playing Tokyo Vol.11</p>
+    </div>
+    <img
+    class="magnifiable"
+    src="https://lh3.googleusercontent.com/eQbzJaZykwFAgN7waDoNyCfEvhJnVbEpV1XTZZKgO06KDK5dp7UXjzCBvzA0EHxKNim5rQ7jPglJndh9Fvz6VCz5Cian-VFPz6fmIJ7Bn_4-1JD8glQqZBN5_Z4I49rRh6WSDf2fGBw=w2400"
+    itemprop="image"
+    width="80%"/>
+    <br>
+    <img
+    class="magnifiable"
+    src="https://lh3.googleusercontent.com/jnRsaIAFty2CGYQZFNJky51zYW8h2iK0_IK4WKHOcNoQsa7voPvMh5FdTxEDveqqueRPynt2A2yUI_sm7YEU6mpPo58XduKfhWXvt0ijjUjogj7HKO8xn3GUcGhvGrptWU3cUDBhhKc=w2400"
+    itemprop="image"
+    width="80%"/>
+    <br>
+    <img
+    class="magnifiable"
+    src="https://lh3.googleusercontent.com/K43Ijd-k4GWZmZU4McXaKerTKSTc85OLlvFz1EHP75vvometc3lV2WGJbh3veJmB5YwzRymyqY189w1gxS-SKNYmt6BWX9_WHIiSHAntTFgUAzBtGMJK-hF9oyp6lqx2LqbAwtmXgt4=w2400"
+    itemprop="image"
+    width="80%"/>
+    <br>
+    <img
+    class="magnifiable"
+    src="https://lh3.googleusercontent.com/jtivmk4SBqQBWeqC3lR014FGa1TqE8fAu_Ne80q5G_NnLq0fJY0JF8ae_FGsmVhyZWUYltRxz3Ng-OVkDJUgMLwAArVLWl_FSpTvyTDnKgIGhd3T_itC6tt1XyaREUY0JWHyMMefAd8=w2400"
+    itemprop="image"
+    width="80%"/>
+    <br>
+        <div class="discription">
+          <p width=60%>
+            <br />
+            2020/09/25に<a href="https://www.twitch.tv/rhizomatiks">twitch.tv/rhizomatiks</a>で配信された<a
+            href="https://twitter.com/rhizomatiks/status/1308730110661414912?s=20"
+            >Playing Tokyo Vol.11</a
+          >
+          でのVJに使用したVisual System．
+          </p>
+        </div>
+        <div class="caption">
+          <p>
+          Tool : TouchDesigner / GLSL
+          </p>
+          <p>
+            Sound :
+          <a href="https://naotokui.net/" target="_blank">Nao Tokui</a
+          >&nbsp;/&nbsp;<a
+            href="https://twitter.com/tukuleord12"
+            target="_blank"
+            >Reo Anzai</a
+          >&nbsp;/&nbsp;<a href="https://twitter.com/yuga_k_dj" target="_blank"
+            >Yuga</a
+          >
+          </p>
+          <p>
+          Link :
+          <a href="https://playing.super-flying.tokyo/" target="_blank"
+            >Playing Tokyo</a
+          >
+          </p>
+          <p>
+            Source Code : <a href="https://github.com/santa-s4nt4/playingTokyo200925" target="_blank">GitHub</a>
+          </p>
+        </div>
+  </div>
+</div>
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500&display=swap");
+
+template {
+  font-family: "JetBrains Mono";
+  font-size: 10px;
+}
+
+.work {
+  width: 60%;
+  padding-bottom: 3rem;
+}
+
+.work p {
+  font-family: "JetBrains Mono";
+  font-weight: 500;
+  display: block;
+  font-size: 1rem;
+  color: #ffffff;
+  letter-spacing: -0.05rem;
+}
+
+.work a {
+  text-decoration: none;
+  color: #9e9e9e;
+}
+
+.work a:hover {
+  color: #ffffff;
+}
+
+.work mark {
+  background-color: #ffffff;
+  font-family: "JetBrains Mono";
+  font-weight: 500;
+  font-size: 0.8rem;
+}
+
+.title p {
+  font-family: "JetBrains Mono";
+  font-weight: 500;
+  display: block;
+  font-size: 1.2rem;
+  color: #ffffff;
+  letter-spacing: -0.05rem;
+}
+
+.discription {
+  padding-bottom: 1.6rem;
+}
+
+.discription p {
+  font-family: 'Noto Sans JP', sans-serif;
+  font-weight: 400;
+  font-size: 1rem;
+  color: #ffffff;
+}
+
+.caption p {
+  padding-bottom: 0.5rem;
+}
+
+.video {
+  position: relative;
+  padding-bottom: 56.25%;
+  height: 0;
+  overflow: hidden;
+}
+
+.video iframe {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+}
+
+</style>
