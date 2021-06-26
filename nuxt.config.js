8789b52522c5a7a163e214ac83e1be50ec1e47e3
santa <santanaruse@gmail.com>
2021-06-26 17:02:56 +0900

    add: titleとfaviconを追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'santa-s4nt4',
+    title: 's4nt4.graphics',
     htmlAttrs: {
       lang: 'en'
     },
@@ -14,7 +14,7 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/santa.jpg' }
     ]
   },
 
