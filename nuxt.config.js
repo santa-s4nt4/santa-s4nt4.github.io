b955e84de13e4cafd0f27530fb5ff73213973100
santa <santanaruse@gmail.com>
2021-11-25 21:12:58 +0900

    fix: profile画像を変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 's4nt4.graphics',
+    title: 's4nt4.cc',
     htmlAttrs: {
       lang: 'en'
     },
